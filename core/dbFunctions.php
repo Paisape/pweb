9f9f27f5802eecff0efc2621a959cc6057cc5acc
Paisape Dev <dev@paisape.in>
2026-08-03 23:33:13 +0530

    Remove raw function text from outside PHP tags in dbFunctions.php

--- core/dbFunctions.php
+++ core/dbFunctions.php
@@ -560,25 +560,3 @@ function obfuscate_email($email) {
 #######################################################################################
 
 ?>
-
-function send_smtp_mail($email_to, $email_subject, $email_message) {
-    require_once 'PHPMailerAutoload.php';
-    @include_once __DIR__ . '/../config.php';
-    $mail = new PHPMailer;
-    $mail->isSMTP();
-    $mail->Host = 'smtp.resend.com';
-    $mail->SMTPAuth = true;
-    $mail->Username = getenv('RESEND_USER') ?: (defined('RESEND_USER') ? RESEND_USER : 'resend');
-    $mail->Password = getenv('RESEND_API_KEY') ?: (defined('RESEND_API_KEY') ? RESEND_API_KEY : '');
-    $mail->SMTPSecure = 'tls';
-    $mail->Port = 587;
-    $mail->setFrom('paispe@mail.paisape.in', 'Paisape Support');
-    $mail->addAddress($email_to);
-    $mail->addReplyTo('hello@paisape.in', 'Paisape Support');
-    $mail->isHTML(true);
-    $mail->Subject = $email_subject;
-    $mail->Body = $email_message;
-    return $mail->send();
-}
-
-
